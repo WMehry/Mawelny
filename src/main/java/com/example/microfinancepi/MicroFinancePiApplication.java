5fd9478ada77a9a41e52b239a68748b857a1e4fa
AnouarMh15 <anouar.mhamdi@esprit.com>
2023-04-07 06:44:44 +0100

    userManagement

--- src/main/java/com/example/microfinancepi/MicroFinancePiApplication.java
+++ src/main/java/com/example/microfinancepi/MicroFinancePiApplication.java
@@ -1,12 +1,14 @@
 package com.example.microfinancepi;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
- @EnableScheduling
-public class MicroFinancePiApplication {
+@EnableScheduling
+@SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT")public class MicroFinancePiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MicroFinancePiApplication.class, args);
